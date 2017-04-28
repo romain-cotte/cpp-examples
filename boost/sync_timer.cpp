96939a6ab22dc9fc48a5caa3c0215d301dcaf851
LilMeyer <romain.cotte@gmail.com>
2017-04-28 23:42:23 +0200

    Add a boost example: blocking wait

---
+++ boost/sync_timer.cpp
@@ -0,0 +1,19 @@
+/**
+ * Please visit http://www.bogotobogo.com/cplusplus/Boost/boost_AsynchIO_asio_tcpip_socket_server_client_timer_A.php
+ */
+#include <iostream>
+#include <boost/asio.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
+
+int main()
+{
+  boost::asio::io_service io;
+
+  for(int i = 0; i < 5; i++) {
+    boost::asio::deadline_timer timer(io, boost::posix_time::seconds(i));
+    timer.wait();
+    std::cout << "Blocking wait(): " << i << " second-wait\n";
+  }
+
+  return 0;
+}
