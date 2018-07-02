93a5bc772cf9ce8f0d3091ae1f82676b0e9ffcdf
Romain Cotte <romain.cotte@gmail.com>
2018-07-02 08:34:46 +0200

    kafka improvements

--- kafka/main.cpp
+++ kafka/main.cpp
@@ -73,7 +73,40 @@ public:
 };
 
 int main(int argc, const char **argv) {
+  std::string brokers = "localhost";
+  std::string errstr;
+  std::string topic_str = "test";
+  std::string mode;
+  std::string debug;
+  int32_t partition = RdKafka::Topic::PARTITION_UA;
+  int64_t start_offset = RdKafka::Topic::OFFSET_BEGINNING;
+
   RdKafka::Conf *conf = RdKafka::Conf::create(RdKafka::Conf::CONF_GLOBAL);
+      topic_str = optarg;
+  RdKafka::Consumer *consumer = RdKafka::Consumer::create(conf, errstr);
+  if (!consumer) {
+    std::cerr << "Failed to create consumer: " << errstr << std::endl;
+    exit(1);
+  }
+  std::cout << "% Created consumer " << consumer->name() << std::endl;
+
+  RdKafka::Topic *topic = RdKafka::Topic::create(consumer,
+                                                 topic_str,
+                                                 tconf,
+                                                 errstr);
+  if (!topic) {
+    std::cerr << "Failed to create topic: " << errstr << std::endl;
+    exit(1);
+  }
+
+  RdKafka::ErrorCode resp = consumer->start(topic, partition, start_offset);
+  if (resp != RdKafka::ERR_NO_ERROR) {
+    std::cerr <<
+      "Failed to start consumer: " <<
+      RdKafka::err2str(resp) << std::endl;
+    exit(1);
+  }
+
 
   return 0;
 }
