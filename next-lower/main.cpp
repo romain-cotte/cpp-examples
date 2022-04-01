3c51f44d11292e0997542ff6649b5eef159eda44
Romain Cotte <romain.cotte@gmail.com>
2022-04-01 12:33:36 +0200

    add next lower, probability

---
+++ next-lower/main.cpp
@@ -0,0 +1,85 @@
+#include <algorithm>
+#include <assert.h>
+#include <bitset>
+#include <cmath>
+#include <cstring>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdio.h>
+#include <string>
+#include <time.h>
+#include <vector>
+
+#define INF 1E9
+#define INF64 2E18
+
+using namespace std;
+
+template<class T1, class T2> void pr(const pair<T1,T2>& x);
+template<class T, size_t SZ> void pr(const array<T,SZ>& x);
+template<class T> void pr(const vector<T>& x);
+template<class T> void pr(const set<T>& x);
+template<class T1, class T2> void pr(const map<T1,T2>& x);
+template<class T1, class T2> void pr(const map<T1,T2>& x);
+template<class... T> void pr(const tuple<T...>& tup);
+
+template<class T> void pr(const T& x) { cout << x; }
+template<class Arg, class... Args> void pr(const Arg& first, const Args&... rest) {
+  pr(first); pr(rest...);
+}
+template<class T1, class T2> void pr(const pair<T1,T2>& x) {
+  pr("{",x.first,", ",x.second,"}");
+}
+template<class T> void prContain(const T& x) {
+  pr("{");bool fst = 1; for(auto &a: x) pr(!fst?", ":"",a), fst = 0;pr("}");
+}
+template<class T, size_t SZ> void pr(const array<T,SZ>& x) { prContain(x); }
+template<class T> void pr(const vector<T>& x) { prContain(x); }
+template<class T> void pr(const set<T>& x) { prContain(x); }
+template<class T1, class T2> void pr(const map<T1,T2>& x) { prContain(x); }
+
+template<class T, size_t... I>
+void pr(const T& tup, index_sequence<I...>) {
+  pr("("); (..., (cout << (I == 0? "" : ", ") << get<I>(tup))); pr(")");
+}
+template<class... T>
+void pr (const tuple<T...>& tup) {
+  pr(tup, make_index_sequence<sizeof...(T)>());
+}
+void ps() { pr("\n"); }
+template<class Arg, class... Args> void ps(const Arg& first, const Args&... rest) {
+  pr(first," "); ps(rest...);
+}
+
+template<typename T> int remin(T& a,const T& b){if(b<a){a=b;return true;}return false;}
+template<typename T> int remax(T& a,const T& b){if(b>a){a=b;return true;}return false;}
+
+typedef vector<int> vi;
+typedef long long ll;
+typedef long double ld;
+typedef vector<vi> vvi;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+
+
+int main(int argc, const char **argv) {
+  vi v = { 5, 10, 7, 15, 3, 4, 23, 17, 10, 2 };
+  int n = v.size();
+  vii s;
+  vi ans(n);
+  for (int i = n-1; i >= 0; --i) {
+    while (!s.empty() && s.back().first > v[i]) s.pop_back();
+    ans[i] = s.empty() ? n: s.back().second;
+    s.push_back(ii(v[i], i));
+  }
+  ps(ans);
+
+  return 0;
+}
---
+++ probability/main.cpp
@@ -0,0 +1,98 @@
+#include <algorithm>
+#include <assert.h>
+#include <bitset>
+#include <cmath>
+#include <cstring>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdio.h>
+#include <string>
+#include <time.h>
+#include <vector>
+
+#define INF 1E9
+#define INF64 2E18
+
+using namespace std;
+
+template<class T1, class T2> void pr(const pair<T1,T2>& x);
+template<class T, size_t SZ> void pr(const array<T,SZ>& x);
+template<class T> void pr(const vector<T>& x);
+template<class T> void pr(const set<T>& x);
+template<class T1, class T2> void pr(const map<T1,T2>& x);
+template<class T1, class T2> void pr(const map<T1,T2>& x);
+template<class... T> void pr(const tuple<T...>& tup);
+
+template<class T> void pr(const T& x) { cout << x; }
+template<class Arg, class... Args> void pr(const Arg& first, const Args&... rest) {
+  pr(first); pr(rest...);
+}
+template<class T1, class T2> void pr(const pair<T1,T2>& x) {
+  pr("{",x.first,", ",x.second,"}");
+}
+template<class T> void prContain(const T& x) {
+  pr("{");bool fst = 1; for(auto &a: x) pr(!fst?", ":"",a), fst = 0;pr("}");
+}
+template<class T, size_t SZ> void pr(const array<T,SZ>& x) { prContain(x); }
+template<class T> void pr(const vector<T>& x) { prContain(x); }
+template<class T> void pr(const set<T>& x) { prContain(x); }
+template<class T1, class T2> void pr(const map<T1,T2>& x) { prContain(x); }
+
+template<class T, size_t... I>
+void pr(const T& tup, index_sequence<I...>) {
+  pr("("); (..., (cout << (I == 0? "" : ", ") << get<I>(tup))); pr(")");
+}
+template<class... T>
+void pr (const tuple<T...>& tup) {
+  pr(tup, make_index_sequence<sizeof...(T)>());
+}
+void ps() { pr("\n"); }
+template<class Arg, class... Args> void ps(const Arg& first, const Args&... rest) {
+  pr(first," "); ps(rest...);
+}
+
+template<typename T> int remin(T& a,const T& b){if(b<a){a=b;return true;}return false;}
+template<typename T> int remax(T& a,const T& b){if(b>a){a=b;return true;}return false;}
+
+typedef vector<int> vi;
+typedef long long ll;
+typedef long double ld;
+typedef vector<vi> vvi;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+
+
+int main(int argc, const char **argv) {
+  ld ev = 0;
+  for (int i = 1; i <= 6; ++i) {
+    // ev += (1-1/36)
+    ev += i-(ld)(2*i-1)/36;
+  }
+  ps(ev);
+  ld ev1 = 0;
+  ev1 += 1 - (ld)1/36;
+  ev1 += 2 - (ld)3/36;
+  ev1 += 3 - (ld)5/36;
+  ev1 += 4 - (ld)7/36;
+  ev1 += 5 - (ld)9/36;
+  ev1 += 6 - (ld)11/36;
+  ps(ev1);
+
+  ld ev2 = 0;
+  ev2 +=(ld)1 * 1/36;
+  ev2 +=(ld)2 * 3/36;
+  ev2 +=(ld)3 * 5/36;
+  ev2 +=(ld)4 * 7/36;
+  ev2 +=(ld)5 * 9/36;
+  ev2 +=(ld)6 * 11/36;
+  ps(ev2);
+
+  return 0;
+}
