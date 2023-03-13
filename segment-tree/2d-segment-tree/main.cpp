eb96670cba4a2eb42a10f3518bd0848fd29d6326
Romain Cotte <romain.cotte@gmail.com>
2023-03-13 21:13:50 +0100

    improve 2d segment tree

--- segment-tree/2d-segment-tree/main.cpp
+++ segment-tree/2d-segment-tree/main.cpp
@@ -27,6 +27,134 @@ using namespace std;
 template<typename T> void remin(T& a, const T& b) { if (b < a) a = b; }
 template<typename T> void remax(T& a, const T& b) { if (b > a) a = b; }
 
+
+int n, m;
+vector<Seg_Tree> vst;
+vector<Seg_Tree> final_segtree;
+
+struct node {
+  bool is_null = false;
+  int val = 0;
+  node set(int v) {
+    val += v;
+    return *this;
+  }
+};
+
+node unite(const node &a, const node &b) {
+  // printf(
+  //   "unite [%d %d] and [%d %d]: %lld and %lld\n",
+  //   a.l, a.r, b.l, b.r, a.val, b.val
+  // );
+  node r;
+  // r.l = a.l; r.r = b.r;
+  r.val = min(a.val, b.val);
+  return r;
+}
+
+/**
+ * Complexity: construction: O(n), query: O(log(n))
+ *       [1;6]
+ *      /     \
+ * [1;3]      [4;6]
+ *   |  \
+ * [1;2] [3;3] ...
+ *   |  \
+ * [1;1] [2;2]
+ *
+ * Pointers are shrinking, we keep as long as intersection is not null
+ * query(3, 5): [1;6] -> [1;3] & [4;6]
+ */
+struct Seg_Tree {
+  int n;
+  vi a;
+  vector<node> tree;
+
+  Seg_Tree(int _n): n(_n) {
+    a.resize(n);
+    tree.resize(4 * n - 1);
+  };
+
+  Seg_Tree(const vi &v): n(v.size()) {
+    a = v;
+    tree.resize(4 * n - 1);
+    build();
+  }
+
+  node build(int x, int l, int r) {
+    if (l == r) { return tree[x].set(a[l]); }
+    int mid = (l + r) / 2;
+    node r1 = build(2 * x + 1,     l, mid);
+    node r2 = build(2 * x + 2, mid+1,  r);
+    return tree[x] = unite(r1, r2);
+  }
+
+  node build() { return build(0, 0, n-1); }
+
+  node query(int x, int nl, int nr, int ql, int qr) {
+    // push(x, nl, nr);
+    if (ql <= nl && nr <= qr) return tree[x];
+    if (qr < nl || nr < ql) return { true };
+    int mid = (nl + nr) / 2;
+    node q1 = query(2 * x + 1, nl   , mid, ql, qr);
+    node q2 = query(2 * x + 2, mid+1,  nr, ql, qr);
+    if (q1.is_null) return q2;
+    if (q2.is_null) return q1;
+    return unite(q1, q2);
+  }
+  node query(int start_idx, int end_idx) {
+    assert(start_idx <= end_idx && start_idx < n && end_idx < n);
+    return query(0, 0, n-1, start_idx, end_idx);
+  }
+};
+
+
+void build_final_segtree(int x, int low, int high) {
+  if (low == high) {
+    final_segtree[x] = vst[low];
+    return;
+  }
+  int mid = (low + high) / 2;
+  build_final_segtree(2*x + 1,   low, mid);
+  build_final_segtree(2*x + 2, mid+1, high);
+
+  for (int k = 0; k < 4 * m-1; ++k) {
+    final_segtree[x].tree[k] = unite(final_segtree[2*x+1].tree[k], final_segtree[2*x+2].tree[k]);
+  }
+}
+
+node final_query(int idx, int x, int start, int end, int x1, int x2) {
+  if (start > x2 || end < x1) return { true };
+  if (x1 <= start && end <= x2) return final_segtree[idx].tree[x];
+  int mid = (start + end) / 2;
+
+  node r1 = final_query(idx, 2*x+1, start, mid, x1, x2);
+  node r2 = final_query(idx, 2*x+2, mid+1, end, x1, x2);
+  if (r1.is_null) return r2;
+  if (r2.is_null) return r1;
+  return unite(r1, r2);
+}
+
+node query(int x, int start, int end, int x1, int y1, int x2, int y2) {
+  if (y2 < start || end < y1) return { true };
+  if (y1 <= start && end <= y2) {
+    auto r = final_query(x, 0, 0, m-1, x1, x2);
+    return r;
+  }
+
+  int mid = (start + end) / 2;
+  node r1 = query(2*x+1, start, mid, x1, y1, x2, y2);
+  node r2 = query(2*x+2, mid+1, end, x1, y1, x2, y2);
+  if (r1.is_null) return r2;
+  if (r2.is_null) return r1;
+  return unite(r1, r2);
+}
+
+int query(int x1, int y1, int x2, int y2) {
+  return query(0, 0, n-1, x1, y1, x2, y2).val;
+}
+
+
 int N;
 
 int mat[4][4] = {
