c6ffc112a7bd7009289e2badfb8e7526e1534e90
Romain Cotte <romain.cotte@gmail.com>
2025-03-25 22:04:16 +0100

    add monotoic buffer resource

---
+++ monotonic-buffer-resource/main.cpp
@@ -0,0 +1,117 @@
+#include <algorithm>
+#include <assert.h>
+#include <bitset>
+#include <cmath>
+#include <cstring>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <memory_resource>
+#include <numeric>
+#include <queue>
+#include <list>
+#include <random>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdio.h>
+#include <string>
+#include <time.h>
+#include <vector>
+
+
+#define INF 2E9
+#define INF64 4E18
+
+using namespace std;
+
+template<class T1, class T2> void pr(const pair<T1,T2>& x);
+template<class T, size_t SZ> void pr(const array<T,SZ>& x);
+template<class T> void pr(const vector<T>& x);
+template<class T> void pr(const deque<T>& x);
+template<class T, class C> void pr(const set<T,C>& x);
+template<class T, class C> void pr(const multiset<T,C>& x);
+template<class T1, class T2> void pr(const map<T1,T2>& x);
+template<class... T> void pr(const tuple<T...>& tup);
+
+const string COLOR_RESET = "\033[0m", GREEN = "\033[1;32m";
+template<class T> void pr(const T& x) {
+#if COLOR == true
+  cout << GREEN << x << COLOR_RESET;
+#else
+  cout << x;
+#endif
+}
+
+template<class Arg, class... Args> void pr(const Arg& first, const Args&... rest) {
+  pr(first); pr(rest...);
+}
+template<class T1, class T2> void pr(const pair<T1,T2>& x) {
+  pr("{",x.first,", ",x.second,"}");
+}
+template<class T> void prContain(const T& x) {
+  pr("{");bool fst = 1; for(auto &a: x) pr(!fst?", ":"",a), fst = 0;pr("}");
+}
+template<class T, size_t SZ> void pr(const array<T,SZ>& x) { prContain(x); }
+template<class T> void pr(const vector<T>& x) { prContain(x); }
+template<class T> void pr(const deque<T>& x) { prContain(x); }
+template<class T, class C> void pr(const set<T,C>& x) { prContain(x); }
+template<class T, class C> void pr(const multiset<T,C>& x) { prContain(x); }
+template<class T1, class T2> void pr(const map<T1,T2>& x) { prContain(x); }
+
+template<class T, size_t... I>
+void pr(const T& tup, index_sequence<I...>) {
+  pr("("); (..., (cout << (I == 0? "" : ", ") << get<I>(tup))); pr(")");
+}
+template<class... T>
+void pr (const tuple<T...>& tup) {
+  pr(tup, make_index_sequence<sizeof...(T)>());
+}
+void ps() { pr("\n"); }
+void _ps_continue() { pr("\n"); }
+template<class Arg, class... Args> void _ps_continue(const Arg& first, const Args&... rest) {
+  pr(" ", first); _ps_continue(rest...);
+}
+
+#ifdef DEBUG_LOCAL
+template<class Arg, class... Args> void ps(const Arg& first, const Args&... rest) {
+  pr(first); _ps_continue(rest...);
+}
+#else
+template<class Arg, class... Args> void ps(const Arg& first, const Args&... rest) {}
+#endif
+
+template<typename T> int remin(T& a,const T& b){if(b<a){a=b;return true;}return false;}
+template<typename T> int remax(T& a,const T& b){if(a<b){a=b;return true;}return false;}
+
+typedef vector<int> vi;
+typedef long long ll;
+typedef long double ld;
+typedef vector<vi> vvi;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+
+
+
+int main(int argc, const char **argv) {
+
+
+  // size 4 Bytes for int, 8 for the padding, 8 for next pointer
+  // 8 for previous pointer => 24 Bytes but it's preferable
+  // to have 32 for the alignment
+  array<byte, 24> buffer;
+
+  pmr::monotonic_buffer_resource mbr{
+    buffer.data(),
+    buffer.size(),
+    pmr::null_memory_resource() // prevent to allocate dynamically if
+                                // not enough memory is available
+  };
+  pmr::polymorphic_allocator<int> pa{&mbr};
+  pmr::list<int> list{pa};
+  list.push_back(1 << 30);
+  cout << list.back() << endl;
+
+
+  return 0;
+}
