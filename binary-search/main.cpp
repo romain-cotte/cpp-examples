865bf5547abf07890910637cd09bb356f90925b0
Romain Cotte <romain.cotte@gmail.com>
2025-12-23 11:40:24 +0100

    binary search

--- binary-search/main.cpp
+++ binary-search/main.cpp
@@ -74,12 +74,12 @@ int n;
 int binary_search(const vi &v, int k) {
   int l = 0, r = v.size()-1;
   while (l < r) {
-    int mid = (l+r+1)/2;
-    // ps(l, r, mid);
-    if (v[mid] <= k) { // last index lower or equal to k
-      l = mid;
+    int m = (l+r+1)/2;
+    // ps(l, r, m);
+    if (v[m] <= k) { // last index lower or equal to k
+      l = m;
     } else {
-      r = mid-1;
+      r = m-1;
     }
   }
   return l;
@@ -89,12 +89,12 @@ int binary_search(const vi &v, int k) {
 int binary_search2(const vi &v, int k) {
   int l = 0, r = v.size()-1;
   while (l < r) {
-    int mid = (l+r)/2;
-    // ps(l, r, mid);
-    if (v[mid] < k) { // first index greater or equal to k
-      l = mid + 1;
+    int m = (l+r)/2;
+    // ps(l, r, m);
+    if (v[m] < k) { // first index greater or equal to k
+      l = m + 1;
     } else {
-      r = mid;
+      r = m;
     }
   }
   return l;
@@ -109,7 +109,29 @@ int main(int argc, const char **argv) {
     int i = binary_search(v, x);
     int j = binary_search2(v, x);
     ps(x, i, j);
-    assert(i == j);
+    assert(i <= j);
   }
+
+  /**
+   * When l < 0 and r > 0, you need to be very careful because
+   * if you do (l+r)/2 when l+r < 0, the rounding is incorrect!
+   */
+
+  for (int x = -10; x <= 10; ++x) {
+    int l = -10, r = 10;
+    while (l < r) {
+      int m = l+(r-l+1)/2;
+      if (m <= x) {
+        l = m;
+      } else {
+        r = m-1;
+      }
+    }
+    ps(x, l);
+    assert(l == x);
+  }
+
+
+
   return 0;
 }
